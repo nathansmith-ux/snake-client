5cd39ec1a5631ea38a773291a6d2d8b9afc7935d
Nathan <nathanrmsmith@gmail.com>
2023-08-07 15:48:50 -0400

    Add setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -2,3 +2,27 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+/**
+ * Callback Function that registers command line keyboard inputs
+ * @param {string} key 
+ * @returns Keyboard letter typed or escape command
+ */
+const handleUserInput = function (key) {
+  console.log("Keyboard input is: ", key);
+  if(key === '\u0003') {
+    process.exit()
+  }
+};
+
+setupInput();
