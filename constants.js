845f4896d2fbb82a51dc3fb04ec8408056c35cd4
Nathan <nathanrmsmith@gmail.com>
2023-08-07 19:37:24 -0400

    Create constants file and imported into different files

---
+++ constants.js
@@ -0,0 +1,25 @@
+const IP = "localhost";
+const PORT = 50541;
+const playerName = "N8S";
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+
+const keyMappings = {
+  f: "Say: You're doing amazing!",
+  e: "Say: Will I ever be full?",
+  q: "Say: Ahhh a wall!",
+  x: "Say: You're getting big!"
+}
+
+module.exports = {
+  IP,
+  PORT,
+  playerName,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  keyMappings
+}
